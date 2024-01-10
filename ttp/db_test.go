dc213ce12b1d4880665cbdcbd708bf614c6c1d95
akakou <akakou571@gmail.com>
2024-01-10 22:26:48 +0900

    test: ct log audit db

--- ttp/db_test.go
+++ ttp/db_test.go
@@ -7,13 +7,7 @@ import (
 	"github.com/go-playground/assert/v2"
 )
 
-func TestDB(t *testing.T) {
-	expected := core.TAInfo{
-		Domain:      "ta.example.com",
-		PublicKey:   []byte("public key"),
-		Attestation: "attestation",
-	}
-
+func makeTestDB() *ttpDB {
 	dbConfig := DBConfig{
 		Type:   "sqlite3",
 		Config: "file:ent?mode=memory&cache=shared&_fk=1",
@@ -25,17 +19,66 @@ func TestDB(t *testing.T) {
 		panic(err)
 	}
 
-	err = db.store(&expected)
+	return db
+
+}
+
+func TestTAInfoDB(t *testing.T) {
+	TADomain := "ta.example.com"
+
+	expected := core.TAInfo{
+		Domain:      "ta.example.com",
+		PublicKey:   []byte("public key"),
+		Attestation: "attestation",
+	}
+
+	db := makeTestDB()
+
+	e := db.toEntTaInfo(&expected)
+	e.SaveX(*db.ctx)
+
+	taInfo, err := db.selectTaInfoByDomain(TADomain)
 
 	if err != nil {
 		panic(err)
 	}
 
-	actual, err := db.findByDomain("ta.example.com")
-
-	if err != nil {
-		panic(err)
-	}
+	actual := db.toCoreTaInfo(taInfo)
 
 	assert.Equal(t, expected, *actual)
+}
+
+func TestCTLogAuditDB(t *testing.T) {
+	TADomain := "ta.example.com"
+
+	expected := CTLogAudit{
+		IsValid:    true,
+		LatestCTId: "",
+		TADomain:   TADomain,
+	}
+
+	taInfo := core.TAInfo{
+		Attestation: "",
+		PublicKey:   []byte{},
+		Domain:      TADomain,
+	}
+
+	db := makeTestDB()
+
+	entTaInfo := db.toEntTaInfo(&taInfo)
+	entTaInfo.SaveX(*db.ctx)
+
+	entCTLogAuditCreate, err := db.toEntCTLogAuditWithRelation(&expected)
+	if err != nil {
+		panic(err)
+	}
+
+	entCTLogAuditCreate.SaveX(*db.ctx)
+	ctLogAudit, err := db.selectCTLogByDomain(TADomain)
+	if err != nil {
+		panic(err)
+	}
+
+	actual := db.toCoreCTLogAudit(ctLogAudit)
+	assert.Equal(t, expected, actual)
 }
