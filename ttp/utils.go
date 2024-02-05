d5d1bd887ffadbc754abdcb43c421049b3e1982b
akakou <akakou571@gmail.com>
2024-02-06 00:46:03 +0900

    feat: check domain violation

--- ttp/utils.go
+++ ttp/utils.go
@@ -2,6 +2,7 @@ package ttp
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/akakou/metact"
 )
@@ -14,4 +15,37 @@ func findCertExtensions(extensions []metact.KeyValue, label string) (string, err
 	}
 
 	return "", errors.New("extension not found")
+}
+
+func splitDomainLast(domain string) string {
+	domain = strings.Replace(domain, "*", "", -1)
+	splited := strings.Split(domain, ".")
+
+	var indexInt int
+	if len(splited) >= 2 {
+		indexInt = 2
+	} else {
+		indexInt = 1
+	}
+
+	last := splited[len(splited)-indexInt:]
+	lastDomain := strings.Join(last, ".")
+
+	return lastDomain
+}
+
+func checkDomainValidation(domains []string) (string, []string, error) {
+	if len(domains) == 0 {
+		return domains[0], []string{}, nil
+	}
+
+	violatingDomains := []string{}
+
+	for _, domain := range domains {
+		domain := splitDomainLast(domain)
+		violatingDomains = append(violatingDomains, domain)
+	}
+
+	return "", violatingDomains, errors.New("domain violation")
+
 }
