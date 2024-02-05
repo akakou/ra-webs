10c1ff7d7e958a04413a1da83ca9ab60fe37a36c
akakou <akakou571@gmail.com>
2024-02-05 17:14:45 +0900

    feat: remove database module

--- ttp/db.go
+++ ttp/db.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/akakou/ra_webs/ttp/ent"
-	"github.com/akakou/ra_webs/ttp/ent/tainfo"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -19,7 +18,7 @@ type ttpDB struct {
 	ctx    *context.Context
 }
 
-func newTtpDB(dbConfig *DBConfig) (*ttpDB, error) {
+func newTTPDB(dbConfig *DBConfig) (*ttpDB, error) {
 	client, err := ent.Open(dbConfig.Type, dbConfig.Config)
 	if err != nil {
 		return nil, fmt.Errorf("failed opening connection to sqlite: %w", err)
@@ -35,68 +34,6 @@ func newTtpDB(dbConfig *DBConfig) (*ttpDB, error) {
 		client: client,
 		ctx:    &ctx,
 	}, nil
-}
-
-func (db *ttpDB) toEntTaInfo(taInfo *TAInfo) *ent.TAInfoCreate {
-	entTaInfo := db.client.TAInfo.
-		Create().
-		SetDomain(taInfo.Domain).
-		SetPublicKeyHash(taInfo.PublicKeyHash).
-		SetAttestation(taInfo.Attestation)
-
-	return entTaInfo
-}
-
-func (db *ttpDB) toCoreTaInfo(taInfo *ent.TAInfo) *TAInfo {
-	return &TAInfo{
-		Domain:        taInfo.Domain,
-		PublicKeyHash: taInfo.PublicKeyHash,
-		Attestation:   taInfo.Attestation,
-	}
-}
-
-func (db *ttpDB) selectTaInfoByDomain(domain string) (*ent.TAInfo, error) {
-	taInfo, err := db.client.TAInfo.
-		Query().
-		Where(tainfo.DomainEQ(domain)).
-		WithCtLog().
-		Only(*db.ctx)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed querying ta info: %w", err)
-	}
-
-	return taInfo, nil
-}
-
-func (db *ttpDB) toEntCTLogAudit(ctLog *CTLogAudit, taInfo *ent.TAInfo) (*ent.CTLogAuditCreate, error) {
-	entCTLog := db.client.CTLogAudit.
-		Create().
-		SetLatestCtID(ctLog.LatestCTId).
-		SetIsValid(ctLog.IsValid).
-		SetTaInfo(taInfo)
-
-	return entCTLog, nil
-}
-
-func (db *ttpDB) toCoreCTLogAudit(ctLogAudit *ent.CTLogAudit) *CTLogAudit {
-	return &CTLogAudit{
-		TADomain:   ctLogAudit.Edges.TaInfo.Domain,
-		IsValid:    ctLogAudit.IsValid,
-		LatestCTId: ctLogAudit.LatestCtID,
-	}
-}
-
-func (db *ttpDB) selectCTLogByDomain(domain string) (*ent.CTLogAudit, error) {
-	taInfo, err := db.selectTaInfoByDomain(domain)
-	if err != nil {
-		return nil, fmt.Errorf("failed to select ta info by domain: %w", err)
-	}
-
-	ctLog := taInfo.Edges.CtLog
-	ctLog.Edges.TaInfo = taInfo
-
-	return ctLog, err
 }
 
 func (db *ttpDB) close() {
