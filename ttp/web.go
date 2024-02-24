5ae115061093a1d7fb8546325eadc0775209c6e0
akakou <akakou571@gmail.com>
2024-02-24 17:02:25 +0900

    feat: validate referer

--- ttp/web.go
+++ ttp/web.go
@@ -3,6 +3,7 @@ package ttp
 import (
 	"net/http"
 
+	"github.com/akakou/ra_webs/ttp/ent/taserver"
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,6 +13,20 @@ var redirectWebPage = echoRoute{
 	f: func(auditor *Auditor) echoRouteFunc {
 		return func(c echo.Context) error {
 			back := c.Request().Header.Get("Referer")
+
+			server, err := auditor.db.Client.TAServer.Query().Where(taserver.DomainEQ(back)).WithTa().First(*auditor.db.Ctx)
+			if err != nil {
+				c.Error(err)
+				return err
+			}
+
+			if server.Edges.Ta == nil {
+				return c.String(http.StatusUnauthorized, "ta is not found")
+			}
+
+			if !server.Edges.Ta.IsValid {
+				return c.String(http.StatusUnauthorized, "server is not valid")
+			}
 
 			return c.Render(http.StatusOK, "redirect", back)
 		}
