40ece6df345809f6a41e1b49d1323b80b3b77935
akakou <akakou571@gmail.com>
2024-04-10 01:45:50 +0900

    refactor: move db to core

--- ttp/ct/audit_test.go
+++ ttp/ct/audit_test.go
@@ -6,7 +6,6 @@ import (
 
 	golangutils "github.com/akakou/golang-utils"
 	"github.com/akakou/ra_webs/ttp/core"
-	"github.com/akakou/ra_webs/ttp/db"
 	"github.com/edgelesssys/ego/attestation"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,12 +14,12 @@ func exampleTTP(t *testing.T) *core.TTP {
 	dbType := golangutils.GetEnv("DB_TYPE", "sqlite3")
 	dbConfig := golangutils.GetEnv("DB_CONFIG", "file:ent?mode=memory&cache=shared&_fk=1")
 
-	dbc := db.DBConfig{
+	dbc := core.DBConfig{
 		Type:   dbType,
 		Config: dbConfig,
 	}
 
-	db, err := db.NewDB(&dbc)
+	db, err := core.NewDB(&dbc)
 	assert.NoError(t, err)
 
 	ttp, err := core.NewTTP(db, nil, "")
