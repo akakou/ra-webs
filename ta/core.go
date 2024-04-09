22c906c11435eb07e36ca9d2ca87f00bd25fb3f2
akakou <akakou571@gmail.com>
2024-04-09 21:21:08 +0900

    fix: remove unused environments variable

--- ta/core.go
+++ ta/core.go
@@ -4,14 +4,9 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
-	"os"
-	"strconv"
 
 	"golang.org/x/crypto/acme/autocert"
 )
-
-const TOKEN_NAME = "RA_WEBS_TOKEN"
-const SERVER_ID_NAME = "RA_WEBS_SERVER_ID"
 
 type Config struct {
 	ServerId int
@@ -25,21 +20,8 @@ type TA struct {
 }
 
 func DefaultConfig() (*Config, error) {
-	token := os.Getenv(TOKEN_NAME)
-	if token == "" {
-		return nil, fmt.Errorf("token not found")
-	}
-
-	s := os.Getenv(SERVER_ID_NAME)
-	serverId, err := strconv.Atoi(s)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse server id: %w", err)
-	}
-
 	return &Config{
-		ServerId: serverId,
-		Token:    token,
-		ACMEUrl:  autocert.DefaultACMEDirectory,
+		ACMEUrl: autocert.DefaultACMEDirectory,
 	}, nil
 }
 
