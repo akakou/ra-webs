7fbc57514fab6dfb523834b96fcf9fba8512d9b8
akakou <akakou571@gmail.com>
2024-04-10 01:49:30 +0900

    refactor: make error consts

--- ttp/core/errors.go
+++ ttp/core/errors.go
@@ -3,4 +3,6 @@ package core
 const (
 	ERROR_INIT_DB         = "failed to initialize DB"
 	ERROR_RANDOM_GENERATE = "failed to generate admin token"
+	ERROR_OPEN_DB         = "failed to open DB"
+	ERROR_CREATE_SCHEMA   = "failed to create schema in DB"
 )
