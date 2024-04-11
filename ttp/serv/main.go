c3f56fcbb871bedd67c2ddd2ddc38a5e3768c8c1
akakou <akakou571@gmail.com>
2024-04-12 00:28:08 +0900

    refactor: move web site code to /web

--- ttp/serv/main.go
+++ ttp/serv/main.go
@@ -1,14 +1,56 @@
 package main
 
-import "github.com/akakou/ra_webs/ttp"
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"io"
+
+	extractembed "github.com/akakou/extract-embed"
+	"github.com/akakou/ra_webs/ttp"
+	"github.com/labstack/echo/v4"
+)
 
 const PORT = ":8081"
 
+//go:embed views/*.html
+var embedFiles embed.FS
+
+const TMP_FOLDER_NAME = "views"
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
-	e, err := ttp.DefaultTTPServer("./views/*.html")
+
+	embeded, err := extractembed.ExtractFs(TMP_FOLDER_NAME, &embedFiles)
 	if err != nil {
 		panic(err)
 	}
+
+	defer embeded.Close()
+
+	fmt.Printf("extracted: %s\n", embeded.Path)
+
+	tmpPath := fmt.Sprintf("%s/%s", embeded.Path, "views/*.html")
+
+	e, err := ttp.DefaultTTPServer()
+	if err != nil {
+		panic(err)
+	}
+
+	e.Renderer = &Template{
+		templates: template.Must(template.ParseGlob(tmpPath)),
+	}
+
+	e.GET("/redirect", func(c echo.Context) error {
+		return c.Render(200, "redirect.html", nil)
+	})
 
 	e.Debug = true
 	e.Logger.Fatal(e.Start(PORT))
