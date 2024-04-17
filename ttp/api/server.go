06c8a534b63eb0db16e8cb3bf168346219c72126
akakou <akakou571@gmail.com>
2024-04-18 05:08:25 +0900

    feat: subscribe ct

--- ttp/api/server.go
+++ ttp/api/server.go
@@ -3,9 +3,11 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 
 	goutils "github.com/akakou/go-utils"
+	"github.com/akakou/ra_webs/core"
 	ttpcore "github.com/akakou/ra_webs/ttp/core"
 	"github.com/akakou/ra_webs/ttp/ent/taserver"
 	"github.com/labstack/echo/v4"
@@ -34,6 +36,15 @@ var PostServerApi = goutils.EchoRoute[ttpcore.TTP]{
 			err = authenticateDomain(req.Domain, service.Token, req.Nonce)
 			if err != nil {
 				return c.String(http.StatusUnauthorized, err.Error())
+			}
+
+			if !core.DEBUG {
+				err = ttp.CT.Subscribe(req.Domain)
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "failed to subscribe %s due to %v", req.Domain, err)
+					return c.String(http.StatusUnauthorized, err.Error())
+				}
 			}
 
 			taServerCreate := ttp.DB.Client.TAServer.
