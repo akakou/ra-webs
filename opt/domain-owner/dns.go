28f32896f939da96a0dfe402d5275a73a33b2922
akakou <akakou571@gmail.com>
2024-04-18 23:11:40 +0900

    feat: add neccesary functions

--- opt/domain-owner/dns.go
+++ opt/domain-owner/dns.go
@@ -1,4 +1,4 @@
-package dns
+package domainowner
 
 import (
 	"errors"
---
+++ opt/domain-owner/memory.go
@@ -0,0 +1,30 @@
+package domainowner
+
+import "strings"
+
+type RecordHolder interface {
+	Append(fqdn string, ip string) error
+	Query(fqdn string) (string, error)
+}
+
+type InMemory map[string]string
+
+func NewInMemory() InMemory {
+	return InMemory{}
+}
+
+func (s InMemory) Append(fqdn string, ip string) error {
+	lower := strings.ToLower(fqdn)
+	s[lower] = ip
+	return nil
+}
+
+func (s InMemory) Query(fqdn string) (string, error) {
+	lower := strings.ToLower(fqdn)
+	ip, ok := s[lower]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return ip, nil
+}
