80c68713be8d5bc0070d8ec5924c38a71094f161
akakou <akakou571@gmail.com>
2024-05-03 21:57:36 +0900

    fix: add export

--- service/cmd/example.sh
+++ service/cmd/example.sh
@@ -2,7 +2,7 @@ echo 'enter admin token: '
 read RA_WEBS_ADMIN_TOKEN
 
 TTP_URL=http://localhost:8000
-RA_WEBS_SERVICE_TOKEN=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $RA_WEBS_ADMIN_TOKEN" $TTP_URL/service)
+export RA_WEBS_SERVICE_TOKEN=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $RA_WEBS_ADMIN_TOKEN" $TTP_URL/service)
 
 go run main.go code https://github.com/akakou-docs/ego-statistical-analysis
 go run main.go server localhost 
