8bdeb06ee1363535516e6f0724cf63f42a2c8a91
akakou <akakou571@gmail.com>
2024-05-03 21:18:15 +0900

    feat: make example showing all flow

---
+++ service/cmd/example.sh
@@ -0,0 +1,8 @@
+echo 'enter admin token: '
+read RA_WEBS_ADMIN_TOKEN
+
+TTP_URL=http://localhost:8000
+RA_WEBS_SERVICE_TOKEN=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $RA_WEBS_ADMIN_TOKEN" $TTP_URL/service)
+
+go run main.go code https://github.com/akakou-docs/ego-statistical-analysis
+go run main.go server localhost 
