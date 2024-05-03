a6305e06b0762011890833e57b34827d6a1224ec
akakou <akakou571@gmail.com>
2024-05-03 20:33:41 +0900

    feat: show token

--- service/cmd/issue_service.sh
+++ service/cmd/issue_service.sh
@@ -1,3 +1,4 @@
 read RA_WEBS_ADMIN_TOKEN
 TTP_URL=http://localhost:8000
 export RA_WEBS_SERVICE_TOKEN=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $RA_WEBS_ADMIN_TOKEN" $TTP_URL/service)
+echo $RA_WEBS_SERVICE_TOKEN
