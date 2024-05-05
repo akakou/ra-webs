6bc82e0b594ef30b1a17d178caf2d8f039eb6b0f
akakou <akakou571@gmail.com>
2024-05-06 03:31:05 +0900

    fix: send yes to git command

--- ttp/builder/build.sh
+++ ttp/builder/build.sh
@@ -1,4 +1,4 @@
-git clone $1 repo --depth 1 --branch main --single-branch --quiet
+echo -en "yes\n" | git clone $1 repo --depth 1 --branch develop --single-branch --quiet
 cd repo
 
 ego-go mod tidy
