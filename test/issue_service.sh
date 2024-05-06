0d60a9ba3cb432ca86a483d33f3c11e544bdecf7
akakou <akakou571@gmail.com>
2024-05-06 09:41:53 +0900

    fix: token miss

--- test/issue_service.sh
+++ test/issue_service.sh
@@ -1,3 +1,3 @@
 ADMIN_TOKEN=""
 TTP_BASE="http://localhost:8000"
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" $TTP_BASE/service
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $ADMIN_TOKEN" $TTP_BASE/service
