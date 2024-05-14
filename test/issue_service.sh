b909291871ec523c00482df3e0c5788e86fb7432
Your Name <you@example.com>
2024-05-14 14:31:14 +0000

    feat: update issue service cmd

--- test/issue_service.sh
+++ test/issue_service.sh
@@ -1,3 +1,3 @@
-ADMIN_TOKEN=""
 TTP_BASE="http://localhost:8000"
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $ADMIN_TOKEN" $TTP_BASE/api/service
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $1" $TTP_BASE/api/service
+echo
