47bc27531a51160cae933da209fbc1ca7979ec07
akakou <akakou571@gmail.com>
2024-05-15 19:44:16 +0900

    fix: update branch

--- ttp/builder/build.sh
+++ ttp/builder/build.sh
@@ -1,4 +1,4 @@
-echo -en "yes\n" | git clone $1 repo --depth 1 --branch develop --single-branch --quiet
+echo -en "yes\n" | git clone $1 repo --depth 1 --branch debug-pub-5-15 --single-branch --quiet
 #echo -en "yes\n" | git clone $1 repo --depth 1 --branch main --single-branch --quiet
 
 cd repo/ta/example
