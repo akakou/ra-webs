0147279daf16ca3b8d29062a76497ef06a6e6feb
akakou <akakou571@gmail.com>
2024-06-26 03:25:55 +0900

    feat: set up notification script

---
+++ ttp/serv/static/js/notify.js
@@ -0,0 +1,45 @@
+const setupNotification = async () => {
+    removeAllNotifcation()
+    const keyResp = await axios.get(`/api/subscription_keys`)
+    console.log(keyResp)
+
+    const permission = await Notification.requestPermission()
+    if (permission !== 'granted') {
+        console.error("Notification is not granted")
+        return
+    }
+
+    const registrations = await navigator.serviceWorker.getRegistrations()
+    if (registrations.length > 0) {
+        console.log("Service worker already registered")
+        return 
+    }
+
+    let subscription
+    try {
+        await navigator.serviceWorker.register('/static/js/sw/service-worker.js', {scope: "/app/redirect/"})
+        const sw = await navigator.serviceWorker.ready;
+
+        subscription = await sw.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: keyResp.data.vapid_public_key,
+        });
+    } catch (e) {
+        console.error("Service worker registration failed: ", e)
+        return
+    }
+
+    const subscribeResp = await axios.post(`/api/subscription`, subscription.toJSON())
+
+    console.log("registered: ", subscribeResp)
+
+}
+
+// this is for debug
+const removeAllNotifcation = async () => {
+    const registrations = await navigator.serviceWorker.getRegistrations()
+    for (const registration of registrations) {
+        await registration.unregister()
+    }
+    console.log("unregistered")
+}
