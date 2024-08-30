3c0cb196745ece35e59e2b788cbf0365f187e36f
akakou <akakou571@gmail.com>
2024-08-30 21:44:53 +0900

    chore: set stable ct logs

--- ttp/audit/main.go
+++ ttp/audit/main.go
@@ -11,8 +11,17 @@ import (
 )
 
 var DefaultCTLogs = []string{
-	"https://oak.ct.letsencrypt.org/2024h1/",
+	"https://ct.googleapis.com/logs/us1/argon2024/",
+	"https://ct.googleapis.com/logs/eu1/xenon2024/",
+	"https://ct.cloudflare.com/logs/nimbus2024/",
+	"https://yeti2024.ct.digicert.com/log/",
+	"https://nessie2024.ct.digicert.com/log/",
+	"https://wyvern.ct.digicert.com/2024h2/",
+	"https://sphinx.ct.digicert.com/2024h2/",
+	"https://sabre2024h2.ct.sectigo.com/",
+	"https://mammoth2024h2.ct.sectigo.com/",
 	"https://oak.ct.letsencrypt.org/2024h2/",
+	"https://ct2024.trustasia.com/log2024/",
 }
 
 type Auditor struct {
