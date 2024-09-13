b2a718f0beadedbca91ae2017751d2c5b6d39b39
akakou <akakou571@gmail.com>
2024-09-14 06:10:24 +0900

    refacor: the register and builder

--- verifier/builder/build_test.go
+++ verifier/builder/build_test.go
@@ -1,15 +1,15 @@
 package builder
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 const EXPECTED_COMMIT_ID = "b9d0bc9519799ae2b07ce033d5dd942d7c275beb"
 const EXPECTED_UNIQUE_ID = "ea0e1b510d48a85981d5a0b3794bb08b75962a67bd44798cdeeaff58745d701a"
 
 func TestMain(t *testing.T) {
-	commitId, uniqueId, err := build("1", "https://github.com/akakou-docs/ego-statistical-analysis")
+	commitId, uniqueId, err := buildCode("1", "https://github.com/akakou-docs/ego-statistical-analysis")
 	fmt.Printf("commit id: %v\nunique id: %v", commitId, uniqueId)
 	if err != nil {
 		t.Fatalf("Error: %v", err)
