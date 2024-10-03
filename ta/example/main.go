8d47028e22fafc3c9662b2035ff88156b596fb34
akakou <akakou571@gmail.com>
2024-10-04 00:07:12 +0900

    refactor: rename REDIRECT => POPUP

--- ta/example/main.go
+++ ta/example/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/akakou/ra_webs/ta"
 )
 
-const REDIRECT_PATH = "/app/verification-status/"
+const POPUP_PATH = "/app/verification-status/"
 
 func main() {
 	config, err := ta.DefaultConfig()
@@ -34,7 +34,7 @@ func main() {
 			fmt.Fprintln(w, `We open verifier....<br/>`)
 			fmt.Fprintln(w, `<script>`)
 			for _, v := range config.Verifiers {
-				fmt.Fprintf(w, `open('%v')`, v+REDIRECT_PATH)
+				fmt.Fprintf(w, `open('%v')`, v+POPUP_PATH)
 			}
 			fmt.Fprintln(w, `</script>`)
 		}
