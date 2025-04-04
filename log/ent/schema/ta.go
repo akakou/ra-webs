8f854291a538d27e8eaedf23b145024186c89497
akakou <akama@keio.jp>
2025-04-04 21:01:58 +0900

    feat: initilize database

---
+++ log/ent/schema/ta.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// TA holds the schema definition for the TA entity.
+type TA struct {
+	ent.Schema
+}
+
+// Fields of the TA.
+func (TA) Fields() []ent.Field {
+	return []ent.Field{
+		field.Bytes("evidence"),
+		field.Bytes("signature"),
+		field.String("repository"),
+		field.String("commit_id"),
+	}
+}
+
+// Edges of the TA.
+func (TA) Edges() []ent.Edge {
+	return nil
+}
