ec1250e9dc6c6a25b580485fd2d34bed1b040954
akakou <akama@keio.jp>
2025-04-05 22:12:36 +0900

    feat: initalize the client

--- monitor/client/example/main.go
+++ monitor/client/example/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
 
+	"github.com/akakou/ra-webs/monitor"
+	"github.com/akakou/ra-webs/monitor/ct/crtsh"
+	browsernotifier "github.com/akakou/ra-webs/monitor/notifier/browser"
+)
+
+func main() {
+	ct, err := crtsh.Default(context.Background())
+
+	if err != nil {
+		panic(err)
+	}
+
+	notifier, err := browsernotifier.Default()
+	if err != nil {
+		panic(err)
+	}
+
+	m, err := monitor.Default(ct, notifier)
+	if err != nil {
+		panic(err)
+	}
+
+	defer m.Close()
+
+	m.Run()
 }
