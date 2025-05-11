083373e76d3ce1f4daa9042046d7569d71da196a
akakou <akama@keio.jp>
2025-05-12 02:00:40 +0900

    refactor: rename SERVICE_TOKEN => RA_WEBS_SERVICE_TOKEN

--- test/notify.sh
+++ test/notify.sh
@@ -1,6 +1,6 @@
-# SERVICE_TOKEN=""
+# RA_WEBS_SERVICE_TOKEN=""
 # MONITOR_BASE=""
 
 TITLE=$1
 MESSAGE=$2
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $SERVICE_TOKEN" $MONITOR_BASE/api/notify -d "{\"message\" : \"$TITLE\n$MESSAGE\"}"
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $RA_WEBS_SERVICE_TOKEN" $MONITOR_BASE/api/notify -d "{\"message\" : \"$TITLE\n$MESSAGE\"}"
