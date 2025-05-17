df2c45e2f52fd224a481e21570bf4b5909e89030
Kosei Akama <akama@keio.jp>
2025-05-18 01:40:42 +0900

    fix: panic db storing fail

--- monitor/register.go
+++ monitor/register.go
@@ -5,48 +5,35 @@ import (
 	"github.com/akakou/ra-webs/monitor/serviceclient"
 )
 
-func (monitor *Monitor) RegisterTA(publicKey []byte) (*ent.TA, error) {
-	taCreate := monitor.DB.Client.TA.
+func (monitor *Monitor) RegisterTA(publicKey []byte) *ent.TA {
+	ta := monitor.DB.Client.TA.
 		Create().
-		SetPublicKey(publicKey)
+		SetPublicKey(publicKey).
+		SaveX(*monitor.DB.Ctx)
 
-	ta, err := taCreate.Save(*monitor.DB.Ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return ta, nil
+	return ta
 }
 
-func (monitor *Monitor) RegisterCTLog(ctLogId int, ta *ent.TA, isActive bool) (*ent.CTLog, error) {
-	ctLogCreate := monitor.DB.Client.CTLog.
+func (monitor *Monitor) RegisterCTLog(ctLogId int, ta *ent.TA, isActive bool) *ent.CTLog {
+	ctLog := monitor.DB.Client.CTLog.
 		Create().
 		SetMonitorLogID(ctLogId).
 		SetTa(ta).
-		SetIsActive(isActive)
+		SetIsActive(isActive).
+		SaveX(*monitor.DB.Ctx)
 
-	ctLog, err := ctLogCreate.Save(*monitor.DB.Ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return ctLog, nil
+	return ctLog
 }
 
-func (monitor *Monitor) RegisterATLog(uniqueId []byte, entry *serviceclient.EvidenceEntry, ta *ent.TA, active bool) (*ent.ATLog, error) {
-	atLogCreate := monitor.DB.Client.ATLog.
+func (monitor *Monitor) RegisterATLog(uniqueId []byte, entry *serviceclient.EvidenceEntry, ta *ent.TA, active bool) *ent.ATLog {
+	atLog := monitor.DB.Client.ATLog.
 		Create().
 		SetEvidence(entry.Evidence).
 		SetRepository(entry.Repository).
 		SetCommitID(entry.CommitID).
 		SetUniqueID(uniqueId).
-		SetTa(ta)
+		SetTa(ta).
+		SaveX(*monitor.DB.Ctx)
 
-	atLog, err := atLogCreate.Save(*monitor.DB.Ctx)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return atLog, nil
+	return atLog
 }
